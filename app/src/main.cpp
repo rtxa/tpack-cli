931764dfcac82ad7678f74b59f3b274670378ca5
rtxa <usertxa@gmail.com>
2024-10-03 01:34:20 -0300

    Add GenesisSDK library

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include "getypes.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "Usage: imagetotxl <input image> <output image>" << std::endl;
+    GE_RGBA color = {255, 0, 0, 255};
+    std::cout << "Color: " << color.r << ", " << color.g << ", " << color.b << ", " << color.a << std::endl;
     return 0;
 }
