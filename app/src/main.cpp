11b9ac52a0478937e5fdb5afd49624e9bc21f792
rtxa <usertxa@gmail.com>
2024-10-03 04:24:53 -0300

    Update main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
-#include "getypes.h"
+
+#include "genesis.h"
+#include "bitmap.h"
+#include "ram.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "Usage: imagetotxl <input image> <output image>" << std::endl;
-    GE_RGBA color = {255, 0, 0, 255};
-    std::cout << "Color: " << color.r << ", " << color.g << ", " << color.b << ", " << color.a << std::endl;
+    std::string file{ "C:/G3D/Genesis3D/v120/levels/gedit.txl" };
+    geVFile *vfs = geVFile_OpenNewSystem(NULL, GE_VFILE_TYPE_VIRTUAL, file.c_str(), NULL, GE_VFILE_OPEN_READONLY | GE_VFILE_OPEN_DIRECTORY);
     return 0;
 }
